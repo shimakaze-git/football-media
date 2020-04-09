3b5fa09f1ebeba86dff5fea78f87c8e5cc578105
shimakaze-git <shimakaze.soft+github@googlemail.com>
2020-04-10 04:05:04 +0900

    feat(media): 色々と追加.

---
+++ media/components/mediaList.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <!-- <div class="player-container"> -->
+      <!-- <vue-core-video-player src="./your_video_source.mp4"></vue-core-video-player> -->
+    <!-- </div> -->
+    <div class="recommend-list">
+      <a-row>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover"><img src="https://img1.wxzxzj.com/vpc-example-cover-your-name-v.jpg" alt="cover">
+            <div class="date">2016</div>
+            </div>
+            <div class="detail">
+            <div class="title">Your Name</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 01:46:04</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover"><img src="https://img1.wxzxzj.com/vpc-example-cover-CJ7-v.jpg" alt="cover">
+            <div class="date">2008</div>
+            </div>
+            <div class="detail">
+            <div class="title">CJ7</div>
+            <div class="author">Stephen Chow</div>
+            <div class="duration">Time: 00:46:04</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover">
+            <img src="https://img.youtube.com/vi/dv21fcORqQE/hqdefault.jpg" alt="cover">
+            <div class="date">2007</div>
+            </div>
+            <div class="detail">
+            <div class="title">5 Centimeters per Second</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 1:02:35</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover"><img src="https://img1.wxzxzj.com/vpc-example-cover-5cm-v.jpg" alt="cover">
+            <div class="date">2007</div>
+            </div>
+            <div class="detail">
+            <div class="title">5 Centimeters per Second</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 1:02:35</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover"><img src="https://img1.wxzxzj.com/vpc-example-cover-the-garden-v.jpg" alt="cover">
+            <div class="date">2013</div>
+            </div>
+            <div class="detail">
+            <div class="title">The Garden of Words</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 00:46:04</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover">
+            <img src="https://img.youtube.com/vi/dv21fcORqQE/hqdefault.jpg" alt="cover">
+            <div class="date">2007</div>
+            </div>
+            <div class="detail">
+            <div class="title">5 Centimeters per Second</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 1:02:35</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover"><img src="https://img1.wxzxzj.com/vpc-example-cover-5cm-v.jpg" alt="cover">
+            <div class="date">2007</div>
+            </div>
+            <div class="detail">
+            <div class="title">5 Centimeters per Second</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 1:02:35</div>
+            </div>
+          </div>
+        </a-col>
+        <a-col :xs="12" :md="6">
+          <div class="movie-recommended">
+            <div class="cover"><img src="https://img1.wxzxzj.com/vpc-example-cover-your-name-v.jpg" alt="cover">
+            <div class="date">2016</div>
+            </div>
+            <div class="detail">
+            <div class="title">Your Name</div>
+            <div class="author">Makoto Shinkai</div>
+            <div class="duration">Time: 01:46:04</div>
+            </div>
+          </div>
+        </a-col>
+      </a-row>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {}
+}
+</script>
+
+<style scoped>
+@import "@/assets/css/app-core-video-player.css";
+@import "@/assets/css/chunk-vendors.css";
+
+/* 
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+*/
+</style>
--- media/layouts/default.vue
+++ media/layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from '~/layouts/header.vue'
+
+export default {
+  components: {
+    Header
+  },
+  data () {
+    return {}
+  }
+}
+</script>
 
 <style>
 html {
